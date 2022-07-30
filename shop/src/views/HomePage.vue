7614af9f5ad8d5f4f749ddcf1495553b1dc124af
lin-company <172083309@qq.com>
2022-07-30 17:24:03 +0800

    shop

---
+++ shop/src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div class="HomePage">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      title: "欢迎来到商场",
+    };
+  },
+};
+</script>
+
+<style scoped lang="css">
+</style>
