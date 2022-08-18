137b4d1c1c7e6942c489ab3a809f0e61271e60b7
lin-company <172083309@qq.com>
2022-08-18 09:11:35 +0800

    11

--- shop/src/components/ClassCom.vue
+++ shop/src/components/ClassCom.vue
@@ -1,29 +1,71 @@
 <template>
-    <div>
-        
+    <div class="ClassCom">
+        <div class="img">
+
+        </div>
+        我是图片分类导航组件
     </div>
 </template>
 
 <script>
-export default {
-    name: 'ShopClassCom',
+    export default {
+        name: 'ClassCom',
 
-    data() {
-        return {
-            
-        };
-    },
+        data() {
+            return {
+                iconList: [
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                    '../../static/macair.svg',
+                ],
+                titleList: [
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                    'MacBook Air',
+                ],
+                navList:[]
+            };
+        },
 
-    mounted() {
-        
-    },
+        mounted() {
+            // 处理列表数据
+            this = me;
+            me.iconList.forEach((i,index)=>{
+                me.navList.push({
+                    id:'navIcon'+index,
+                    icon:i,
+                    title:me.titleList[index]
+                })
+            })
+            console.log('navList整理后的结果：',me.navList)
+        },
 
-    methods: {
-        
-    },
-};
+        methods: {
+
+        },
+    };
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    .img {
+        background-image: url('../../static/svg/air.svg');
+        width: 80px;
+        height: 54px;
+    }
 </style>
--- shop/src/pages/HomePage.vue
+++ shop/src/pages/HomePage.vue
@@ -2,15 +2,18 @@
 <template>
   <div class="HomePage">
     <NavCom></NavCom>
+    <ClassCom></ClassCom>
   </div>
 </template>
 
 <script>
+import  ClassCom  from "../components/ClassCom";
 import NavCom from '../components/NavCom'
 export default {
   name: "HomePage",
   components:{
-    NavCom
+    NavCom,
+    ClassCom
   },
   data() {
     return {
