1e26f8fa753aa68841b30764bbed0c676cda2153
lin-company <172083309@qq.com>
2022-08-28 17:21:11 +0800

    111

--- shop/src/components/CompareCom.vue
+++ shop/src/components/CompareCom.vue
@@ -9,7 +9,7 @@
           <div
             class="note-mac"
             :style="navStyleWat ? navStyle : ''"
-            @click="navStyleWat = !navStyleWat"
+            @click="switchCpe"
           >
             <span>笔记本电脑</span>
           </div>
@@ -17,14 +17,19 @@
           <div
             class="mac"
             :style="!navStyleWat ? navStyle : ''"
-            @click="navStyleWat = !navStyleWat"
+            @click="switchCpe"
           >
             <span>台式电脑</span>
           </div>
         </div>
       </div>
-      <CpeCom v-show="navStyleWat"></CpeCom>
-      <div v-show="!navStyleWat">12121221</div>
+      <CpeCom class="cpe1" :style="cpeS1" :pmrList="noteMac"></CpeCom>
+      <CpeCom class="cpe2" :style="cpeS2" :pmrList="desktopMac"></CpeCom>
+      <!-- 查看更多 -->
+      <div class="look-move">
+        <el-link> 比较各款 Mac 机型 > </el-link>
+        <el-link> 选购 Mac > </el-link>
+      </div>
     </div>
   </div>
 </template>
@@ -41,13 +46,255 @@ export default {
     return {
       title: "哪款Mac适合你？",
       navStyle: "opacity: 1;border-bottom:0.1rem solid black;",
+      cpeS1: "opacity: 1",
+      cpeS2: "opacity: 0",
       navStyleWat: 1,
+      // 参数对比
+      noteMac: [
+        {
+          productImg: require("../../static/img/air_mac.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M1芯片机型)",
+          pirce: 7999,
+          size: "13.3英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M1芯片",
+          cpuImg: require("../../static/img/m1.png"),
+          coreQuantity: "8核",
+          max: 0,
+          graphics: "7",
+          graphicsMax: 0,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "16GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "立体声扬声器"],
+          weight: 1.29,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+        {
+          productImg: require("../../static/img/air2_mac.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M2芯片机型)",
+          pirce: 9499,
+          size: "13.6英寸",
+          screen: "Liquid 视网膜显示屏",
+          cpu: "Apple M2芯片",
+          cpuImg: require("../../static/img/m2.png"),
+          coreQuantity: "8核",
+          max: 1,
+          graphics: "10",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "24GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "1080p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "支持空间音频的四扬声器系统"],
+          weight: 1.24,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+        {
+          productImg: require("../../static/img/pro_mac.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Pro 13 英寸",
+          particular: "",
+          pirce: 9999,
+          size: "13.3 英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M2芯片",
+          cpuImg: require("../../static/img/m2.png"),
+          coreQuantity: "8核",
+          max: 0,
+          graphics: "10",
+          graphicsMax: 0,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "24GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "20",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["录音棚级三麦克风阵列", "支持空间音频的立体声扬声器"],
+          weight: 1.38,
+          unlockImg: require("../../static/img/zhiwen_box.png"),
+          unlock: "触控栏和触控 ID",
+        },
+        {
+          productImg: require("../../static/img/proplus_mac.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: " ",
+          name: "MacBook Pro14 英寸和 16 英寸",
+          particular: "",
+          pirce: 14999,
+          size: "14.2 英寸或 16.2 英寸",
+          screen: "Liquid 视网膜 XDR 显示屏",
+          cpu: "Apple M1 Pro 芯片或Apple M1 Max 芯片",
+          cpuImg: require("../../static/img/m1max.png"),
+          coreQuantity: "10 核",
+          max: 1,
+          graphics: "32",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "64GB",
+          storage: "8T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "21",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "1080p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["录音棚级三麦克风阵列", "支持空间音频的六扬声器系统"],
+          weight: "1.6 千克或 2.1 千克",
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+      ],
+      desktopMac: [
+        {
+          productImg: require("../../static/img/compare_imac24__bdgus6d1xqb6_large.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M1芯片机型)",
+          pirce: 7999,
+          size: "13英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M1芯片",
+          cpuImg: require("../../static/img/m1.png"),
+          coreQuantity: "8核",
+          max: 1,
+          graphics: "7",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "16GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "立体声扬声器"],
+          weight: 1.29,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+        {
+          productImg: require("../../static/img/compare_macmini__b77zzeep1s6a_large.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M1芯片机型)",
+          pirce: 7999,
+          size: "13英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M1芯片",
+          cpuImg: require("../../static/img/m1.png"),
+          coreQuantity: "8核",
+          max: 1,
+          graphics: "7",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "16GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "立体声扬声器"],
+          weight: 1.29,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+        {
+          productImg: require("../../static/img/compare_macstudio__dg371ccau70i_large.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M1芯片机型)",
+          pirce: 7999,
+          size: "13英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M1芯片",
+          cpuImg: require("../../static/img/m1.png"),
+          coreQuantity: "8核",
+          max: 1,
+          graphics: "7",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "16GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "立体声扬声器"],
+          weight: 1.29,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+        {
+          productImg: require("../../static/img/compare_macpro__cj3i514iwpua_large.png"),
+          colorImg: require("../../static/img/color.png"),
+          label: "新款",
+          name: "MacBook Air",
+          particular: "(M1芯片机型)",
+          pirce: 7999,
+          size: "13英寸",
+          screen: "视网膜显示屏",
+          cpu: "Apple M1芯片",
+          cpuImg: require("../../static/img/m1.png"),
+          coreQuantity: "8核",
+          max: 1,
+          graphics: "7",
+          graphicsMax: 1,
+          memoryImg: require("../../static/img/compare.png"),
+          memory: "16GB",
+          storage: "2T",
+          batteryImg: require("../../static/img/dianci.png"),
+          battery: "18",
+          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
+          camera: "720p",
+          audioImg: require("../../static/img/yinliang.png"),
+          audio: ["三麦克风阵列", "立体声扬声器"],
+          weight: 1.29,
+          unlockImg: require("../../static/img/zhiwen.png"),
+          unlock: "触控 ID",
+        },
+      ],
     };
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    switchCpe() {
+      this.navStyleWat = !this.navStyleWat;
+      if (this.navStyleWat) {
+        this.cpeS1 = "opacity:1;";
+        this.cpeS2 = "opacity:0;";
+      } else {
+        this.cpeS1 = "opacity:0;";
+        this.cpeS2 = "opacity:1;";
+      }
+    },
+  },
 };
 </script>
 
@@ -61,6 +308,7 @@ export default {
     position: relative;
     left: 50%;
     transform: translateX(-50%);
+    height: 100%;
     .title-box {
       font-size: 4.6rem;
       font-weight: 600;
@@ -97,6 +345,29 @@ export default {
     }
     .nbsp {
       width: 6rem;
+    }
+  }
+  .cpe1 {
+    position: absolute;
+    transition: all 1s;
+    left: 0;
+    top: 20rem;
+  }
+  .cpe2 {
+    position: absolute;
+    transition: all 1s;
+    left: 0;
+    top: 20rem;
+  }
+  .look-move {
+    position: absolute;
+    bottom: 15.5rem;
+    width: 100%;
+    text-align: center;
+    &::v-deep .el-link--inner {
+      font-size: 2.1rem;
+      color: #06c;
+      margin-right:4.2rem;
     }
   }
 }
--- shop/src/components/CpeCom.vue
+++ shop/src/components/CpeCom.vue
@@ -3,11 +3,14 @@
     <div class="cpe-box">
       <el-form v-for="(i, index) in pmrList" :key="index" :model="i">
         <div class="top-box">
-          <el-image class="productImg" :src="i.productImg">
-            <div slot="error" class="image-slot">
-              <i class="el-icon-picture-outline"></i>
-            </div>
-          </el-image>
+          <div class="productImg-box">
+            <el-image class="productImg" :src="i.productImg">
+              <div slot="error" class="image-slot">
+                <i class="el-icon-picture-outline"></i>
+              </div>
+            </el-image>
+          </div>
+
           <el-image class="colorImg" :src="i.colorImg">
             <div slot="error" class="image-slot">
               <i class="el-icon-picture-outline"></i>
@@ -16,12 +19,16 @@
           <div class="label">
             {{ i.label }}
           </div>
-          <div class="name">
-            {{ i.name }}
+
+          <div class="name-box">
+            <div class="name">
+              {{ i.name }}
+            </div>
+            <div class="particular">
+              {{ i.particular }}
+            </div>
           </div>
-          <div class="particular">
-            {{ i.particular }}
-          </div>
+
           <div class="pirce-box">
             RMB&nbsp;
             <div class="pirce">{{ i.pirce }}</div>
@@ -62,7 +69,7 @@
           <div>中央处理器</div>
         </div>
         <div class="graphics-box">
-          <div v-if="i.graphicsMax">最高可选配</div>
+          <div v-show="i.graphicsMax">最高可选配</div>
           <div class="graphics">{{ i.graphics }}核</div>
           <div>图形处理器</div>
         </div>
@@ -95,7 +102,7 @@
           </div>
         </div>
         <div class="weight-box">
-          <div class="weight">{{ i.weight }} 千克</div>
+          <div class="weight">{{ i.weight }}</div>
           <div>重量</div>
         </div>
         <div class="unlock-box">
@@ -112,126 +119,14 @@
 <script>
 export default {
   name: "CpeCom",
+  props: ["pmrList"],
   data() {
-    return {
-      pmrList: [
-        {
-          productImg: require("../../static/img/air_mac.png"),
-          colorImg: require("../../static/img/color.png"),
-          label: "新款",
-          name: "MacBook Air",
-          particular: "(M1芯片机型)",
-          pirce: 7999,
-          size: "13英寸",
-          screen: "视网膜显示屏",
-          cpu: "Apple M1芯片",
-          cpuImg: require("../../static/img/m1.png"),
-          coreQuantity: "8核",
-          max: 1,
-          graphics: "7",
-          graphicsMax: 1,
-          memoryImg: require("../../static/img/compare.png"),
-          memory: "16GB",
-          storage: "2T",
-          batteryImg: require("../../static/img/dianci.png"),
-          battery: "18",
-          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
-          camera: "720p",
-          audioImg: require("../../static/img/yinliang.png"),
-          audio: ["三麦克风阵列", "立体声扬声器"],
-          weight: 1.29,
-          unlockImg: require("../../static/img/zhiwen.png"),
-          unlock: "触控 ID",
-        },
-        {
-          productImg: require("../../static/img/air_mac.png"),
-          colorImg: require("../../static/img/color.png"),
-          label: "新款",
-          name: "MacBook Air",
-          particular: "(M1芯片机型)",
-          pirce: 7999,
-          size: "13英寸",
-          screen: "视网膜显示屏",
-          cpu: "Apple M1芯片",
-          cpuImg: require("../../static/img/m1.png"),
-          coreQuantity: "8核",
-          max: 1,
-          graphics: "7",
-          graphicsMax: 1,
-          memoryImg: require("../../static/img/compare.png"),
-          memory: "16GB",
-          storage: "2T",
-          batteryImg: require("../../static/img/dianci.png"),
-          battery: "18",
-          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
-          camera: "720p",
-          audioImg: require("../../static/img/yinliang.png"),
-          audio: ["三麦克风阵列", "立体声扬声器"],
-          weight: 1.29,
-          unlockImg: require("../../static/img/zhiwen.png"),
-          unlock: "触控 ID",
-        },
-        {
-          productImg: require("../../static/img/air_mac.png"),
-          colorImg: require("../../static/img/color.png"),
-          label: "新款",
-          name: "MacBook Air",
-          particular: "(M1芯片机型)",
-          pirce: 7999,
-          size: "13英寸",
-          screen: "视网膜显示屏",
-          cpu: "Apple M1芯片",
-          cpuImg: require("../../static/img/m1.png"),
-          coreQuantity: "8核",
-          max: 1,
-          graphics: "7",
-          graphicsMax: 1,
-          memoryImg: require("../../static/img/compare.png"),
-          memory: "16GB",
-          storage: "2T",
-          batteryImg: require("../../static/img/dianci.png"),
-          battery: "18",
-          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
-          camera: "720p",
-          audioImg: require("../../static/img/yinliang.png"),
-          audio: ["三麦克风阵列", "立体声扬声器"],
-          weight: 1.29,
-          unlockImg: require("../../static/img/zhiwen.png"),
-          unlock: "触控 ID",
-        },
-        {
-          productImg: require("../../static/img/air_mac.png"),
-          colorImg: require("../../static/img/color.png"),
-          label: "新款",
-          name: "MacBook Air",
-          particular: "(M1芯片机型)",
-          pirce: 7999,
-          size: "13英寸",
-          screen: "视网膜显示屏",
-          cpu: "Apple M1芯片",
-          cpuImg: require("../../static/img/m1.png"),
-          coreQuantity: "8核",
-          max: 1,
-          graphics: "7",
-          graphicsMax: 1,
-          memoryImg: require("../../static/img/compare.png"),
-          memory: "16GB",
-          storage: "2T",
-          batteryImg: require("../../static/img/dianci.png"),
-          battery: "18",
-          cameraImg: require("../../static/img/compare_icon_camera__dlxow9r3leie_large.png"),
-          camera: "720p",
-          audioImg: require("../../static/img/yinliang.png"),
-          audio: ["三麦克风阵列", "立体声扬声器"],
-          weight: 1.29,
-          unlockImg: require("../../static/img/zhiwen.png"),
-          unlock: "触控 ID",
-        },
-      ],
-    };
+    return {};
   },
 
-  mounted() {},
+  mounted() {
+    console.log(this.pmrList, "11111");
+  },
 
   methods: {},
 };
@@ -254,20 +149,31 @@ export default {
   font-weight: 600;
   margin: 0.6rem 0 0.5rem 0;
 }
+.w71 {
+  height: 7.1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 .cpe-box {
+  width: 98rem;
   display: flex;
   justify-content: space-between;
+  
   .el-form > :nth-child(n):not(:nth-child(1)) {
     margin-bottom: 4rem;
   }
   .el-form {
+    border-bottom: 0.1rem solid rgba(0,0,0,0.3);
     letter-spacing: 0.011rem;
     text-align: center;
     width: 21.95rem;
     position: relative;
     font-size: 1.4rem;
     color: #1d1d1f;
-    .productImg {
+    .productImg-box {
+      width: 21.95rem;
+      height: 16.7rem;
     }
     .colorImg {
       margin-top: 2.5rem;
@@ -276,6 +182,10 @@ export default {
     .label {
       color: #bf4800;
       font-size: 1rem;
+      height: 1.6rem;
+    }
+    .name-box {
+      height: 5.6rem;
     }
     .name {
       font-size: 2.4rem;
@@ -322,22 +232,26 @@ export default {
       }
     }
     .cpu-box {
+      height: 7.1rem;
+      .cpuImg{
+       
+      }
     }
-    .coreQuantity-box {
+    .coreQuantity-box:extend(.w71) {
       .coreQuantity {
         font-size: 2.1rem;
         font-weight: 600;
         margin: 0.6rem 0 0.5rem 0;
       }
     }
-    .graphics-box {
+    .graphics-box:extend(.w71) {
       .graphics {
         font-size: 2.1rem;
         font-weight: 600;
         margin: 0.6rem 0 0.5rem 0;
       }
     }
-    .storage-box {
+    .storage-box:extend(.w71) {
       .storage:extend(.fz21-w) {
       }
     }
